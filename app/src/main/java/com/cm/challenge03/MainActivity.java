968466e81bb466bd8527b50c8fa27284518c6e0a
Cláudia Campos <45832743+TaurusLegend@users.noreply.github.com>
2022-11-29 02:28:00 +0000

    Adding Dummy Notification.

--- app/src/main/java/com/cm/challenge03/MainActivity.java
+++ app/src/main/java/com/cm/challenge03/MainActivity.java
@@ -1,6 +1,10 @@
 package com.cm.challenge03;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,12 +12,21 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
 
 import com.cm.challenge03.ui.main.MainFragment;
 import com.cm.challenge03.ui.main.interfaces.FragmentChanger;
 
 public class MainActivity extends AppCompatActivity implements FragmentChanger {
+    private static final int notificationId = 0;
+    private static final String CHANNEL_ID = "test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,17 @@ public class MainActivity extends AppCompatActivity implements FragmentChanger {
         if (savedInstanceState == null) {
             addFragment(MainFragment.class, false);
         }
+
+        /*NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(notificationId, builder.build());*/
     }
 
     @Override
--- app/src/main/java/com/cm/challenge03/ui/main/MainFragment.java
+++ app/src/main/java/com/cm/challenge03/ui/main/MainFragment.java
@@ -1,18 +1,26 @@
 package com.cm.challenge03.ui.main;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 import com.cm.challenge03.R;
 import com.cm.challenge03.ui.main.interfaces.FragmentChanger;
 
 public class MainFragment extends Fragment {
+    private static final int notificationId = 0;
+    private static final String CHANNEL_ID = "temperature";
     private MainViewModel mainViewModel;
 
     @Override
@@ -26,5 +34,39 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            Button button = view.findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        CharSequence name = getString(R.string.channel_name);
+                        String description = getString(R.string.channel_description);
+                        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                        channel.setDescription(description);
+                        // Register the channel with the system; you can't change the importance
+                        // or other notification behaviors after this
+                        NotificationManager notificationManager = requireActivity().getSystemService(NotificationManager.class);
+                        notificationManager.createNotificationChannel(channel);
+                    }
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(requireActivity(), CHANNEL_ID)
+                            .setSmallIcon(R.drawable.outline_warning_24)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setAutoCancel(true);
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireActivity());
+
+                    notificationManager.notify(notificationId, builder.build());
+                }
+            });
+
+        super.onViewCreated(view, savedInstanceState);
     }
 }
