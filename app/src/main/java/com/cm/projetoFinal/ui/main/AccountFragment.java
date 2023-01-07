cdc95a9158212fa0a1690bbb122019cf08111cc9
BeaMadeira <63881025+BeaMadeira@users.noreply.github.com>
2023-01-07 15:58:39 +0000

    account photo stuff

--- app/src/main/java/com/cm/projetoFinal/ui/main/AccountFragment.java
+++ app/src/main/java/com/cm/projetoFinal/ui/main/AccountFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -32,11 +33,31 @@ import android.widget.Toast;
 import com.cm.projetoFinal.MainViewModel;
 import com.cm.projetoFinal.R;
 //import com.cm.projetoFinal.database.entities.Profile;
+import com.cm.projetoFinal.ui.main.interfaces.Authentication;
 import com.cm.projetoFinal.ui.main.interfaces.FragmentChanger;
 import com.cm.projetoFinal.ui.main.interfaces.TaskCallback;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import android.util.Base64;
+
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AccountFragment extends Fragment {
     private MainViewModel mainViewModel;
@@ -79,13 +100,7 @@ public class AccountFragment extends Fragment {
         });
 
 
-        image_button = view.findViewById(R.id.imageButton);
-        image_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageChooser();
-            }
-        });
+
 
         Button saveProfile = view.findViewById(R.id.profile_button);
         saveProfile.setOnClickListener(new View.OnClickListener() {
@@ -143,6 +158,50 @@ public class AccountFragment extends Fragment {
 
                 }
         });*/
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+
+        image_button = view.findViewById(R.id.imageButton);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = ((Authentication) requireActivity()).getCurrentUser();
+        FirebaseStorage.getInstance().getReference("images/"+user.getUid()).getBytes(Long.MAX_VALUE)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                        image_button.setImageBitmap(bitmap);
+                    }
+                });
+/*
+
+        db.collection("users").document(user.getUid()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot!=null && documentSnapshot.exists()){
+                            String imgstring =documentSnapshot.getString("ImageString");
+                            if(imgstring!=null){
+                                byte [] stringbyte = Base64.decode(imgstring, Base64.DEFAULT);
+                                Bitmap bitmap = BitmapFactory.decodeByteArray(stringbyte,0,stringbyte.length);
+                                image_button.setImageBitmap(bitmap);
+                            }
+                        }
+                    }
+                });
+*/
+
+
+
+
+
+        image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
     }
 
     private void imageChooser() {
@@ -165,7 +224,61 @@ public class AccountFragment extends Fragment {
                         Uri selectedImageUri = data.getData();
                         Bitmap selectedImageBitmap = null;
                         try {
+
                             selectedImageBitmap = MediaStore.Images.Media.getBitmap(this.getActivity().getContentResolver(), selectedImageUri);
+                            ByteArrayOutputStream output= new ByteArrayOutputStream();
+                            selectedImageBitmap.compress(Bitmap.CompressFormat.JPEG,100,output);
+                            byte [] imagebyte = output.toByteArray();
+                            String imagestring = Base64.encodeToString(imagebyte, Base64.DEFAULT);
+
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            FirebaseUser user = ((Authentication) requireActivity()).getCurrentUser();
+                            FirebaseStorage.getInstance().getReference("images/"+user.getUid()).putBytes(imagebyte)
+                                    .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                            if(task.isSuccessful()){
+                                                Log.d("DEBUG","SUCCESS IN ADDING");
+
+                                            }
+                                        }
+                                    });
+
+
+                           /* SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("foto_perfil",imagestring);
+                            editor.apply();*/
+                            //FirebaseUser user = ((Authentication) requireActivity()).getCurrentUser();
+                            /*String userid = user.getUid();
+
+                            DocumentReference ref = FirebaseFirestore.getInstance().collection("user").document(userid);
+                            Map<String,Object> updates = new HashMap<>();
+                            updates.put("imageProfile",imagestring);
+                            ref.update(updates);*/
+                            //FirebaseUser user = ((Authentication) requireActivity()).getCurrentUser();
+
+                          /*  Map<String, Object> updates = new HashMap<>();
+                            updates.put("ImageString", imagestring);
+
+                            db.collection("users").document(user.getUid())
+                                    .set(updates, SetOptions.merge())
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            // the update was successful
+                                            Log.d("DEBUG","SUCCESS IN ADDING");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // the update failed
+                                            Log.d("DEBUG","FAILURE IN ADDING");
+                                        }
+                                    });
+*/
+
                         }
                         catch (IOException e) {
                             e.printStackTrace();
